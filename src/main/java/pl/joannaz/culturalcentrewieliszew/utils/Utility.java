074498b698ae385be5ffa7c1fabb8c91e32f5221
Joanna Zawalich <joanna.ewa.zawalich@gmail.com>
2024-04-18 11:44:03 +0200

    DONE: - removeDiacritics fixed (works with 'ł' and 'Ł')
    TODO:
    - AuthEntryPointJWT (exception handler) ?

--- src/main/java/pl/joannaz/culturalcentrewieliszew/utils/Utility.java
+++ src/main/java/pl/joannaz/culturalcentrewieliszew/utils/Utility.java
@@ -7,8 +7,14 @@ public final class Utility {
     private Utility() {}
 
     public static String removeDiacritics(String input) {
-        String normalized = Normalizer.normalize(input, Normalizer.Form.NFD);
+        String normalized = Normalizer.normalize(input, Normalizer.Form.NFKD);
         Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
-        return pattern.matcher(normalized).replaceAll("");
+        String inputWithoutDiacritics = pattern.matcher(normalized).replaceAll("");
+        // Check if inputNormalized contains 'ł' or 'Ł' characters before replacing
+        // (cause: code above does not work for these letters)
+        if (inputWithoutDiacritics.contains("ł") || inputWithoutDiacritics.contains("Ł")) {
+            inputWithoutDiacritics = inputWithoutDiacritics.replace('ł', 'l').replace('Ł', 'L');
+        }
+        return inputWithoutDiacritics;
     }
 }
